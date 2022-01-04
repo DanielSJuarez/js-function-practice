329b55f6e5b1a115a4c93647ee6cb09f5be452a1
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-04 16:16:57 -0500

    removed temporary console log

--- scripts/scriptsExercises.js
+++ scripts/scriptsExercises.js
@@ -87,7 +87,6 @@
         return newText;
     } 
 
-    console.log(rovarspraket("this is fun"));
 
     // ---------------------
     // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
