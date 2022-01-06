99ee06f4ee476ed6df1817753b36a9e3d1c941de
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-06 09:00:25 -0500

    refactor from review

--- scripts/scriptsRefactor.js
+++ scripts/scriptsRefactor.js
@@ -17,274 +17,284 @@
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
-(function() {
-    'use strict';
-  
-  
+(function () {
+  'use strict';
+
+
   // 1. --------------------------------------------
-  
+
   // This is a named function, convert it
   // to a variable containing an anonymous
   // function
-  
-//   function doSomethingCool() {
-//     console.log("Something Cool!");
-//   }
-  
+
+  //   function doSomethingCool() {
+  //     console.log("Something Cool!");
+  //   }
+
   // Put your answer below -------------------------
-  let doSomethingCool = function() {
+  let doSomethingCool = function () {
     console.log("Something Cool!");
   }
-  
+
   // -----------------------------------------------
 
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 2. --------------------------------------------
-  
+
   // Here we are using setTimeout to call a function
   // after 2 seconds. Refactor to use an anonymous
   // function
-  
-//   function sayHi() {
-//     alert("Hello, World!");
-//   }
-  
-//   setTimeout(sayHi, 2000);
-  
+
+  //   function sayHi() {
+  //     alert("Hello, World!");
+  //   }
+
+  //   setTimeout(sayHi, 2000);
+
   // Put your answer below -------------------------
-  
-  let sayHi = function(){
+
+  let sayHi = function () {
     alert("Hello, World!");
   }
-  
+
   setTimeout(sayHi(2000));
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 3. --------------------------------------------
-  
+
   // The code below will log the letter twice. What
   // is the order that will be logged?
-  
+
   // [a]: x then y then z
   // [b]: y then z
   // [c]: z then y
   // [d]: x then z
-  
+
   // Please explain your answer.
-  
+
   var letter = "x";
-  
-  setTimeout(function(){
+
+  setTimeout(function () {
     letter = "y";
     console.log("The letter is", letter);
   }, 1);
-  
+
   letter = "z";
   console.log("The letter is", letter);
-  
+
   // Put your answer below -------------------------
 
   //Option [c] as the var letter with 'x' is not being called. Then being replaced with setTimeout function to 'y', then being console.logged once the function finshes. However, this finishes after the var letter is being replaced outside the function with 'z' due to the setTimout method being call. Thus 'z' then 'y' is logged.
-  
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 4. --------------------------------------------
-  
+
   // The function below reverses a string. The body
   // of the function is 5 lines of code. Refactor
   // this function to do the same thing with 1 line
-  
-//   var reverseStr = function(str) {
-//     var arr;
-//     arr = str.split("");
-//     arr = arr.reverse();
-//     str = arr.join("");
-//     return str;
-//   };
-  
+
+  //   var reverseStr = function(str) {
+  //     var arr;
+  //     arr = str.split("");
+  //     arr = arr.reverse();
+  //     str = arr.join("");
+  //     return str;
+  //   };
+
   // Put your answer below -------------------------
   var reverseStr = (str) => str.split('').reverse().join('');
 
-  
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 5. --------------------------------------------
-  
+
   // The function below takes the spanish word for
   // the colors red, white, blue, green, and black
   // and returns the hex code for that color.
   // Refactor this function to use an object
   // instead of an if/else statement.
-  
-//   var spanishColor = function(colorName) {
-//     if (colorName.toLowerCase() === "rojo") {
-//       return "#ff0000";
-//     }
-//     else if (colorName.toLowerCase() === "blanco") {
-//       return "#ffffff";
-//     }
-//     else if (colorName.toLowerCase() === "azul") {
-//       return "#0000ff";
-//     }
-//     else if (colorName.toLowerCase() === "verde") {
-//       return "#00ff00";
-//     }
-//     else if (colorName.toLowerCase() === "negro") {
-//       return "#000000";
-//     }
-//   };
-  
+
+  //   var spanishColor = function(colorName) {
+  //     if (colorName.toLowerCase() === "rojo") {
+  //       return "#ff0000";
+  //     }
+  //     else if (colorName.toLowerCase() === "blanco") {
+  //       return "#ffffff";
+  //     }
+  //     else if (colorName.toLowerCase() === "azul") {
+  //       return "#0000ff";
+  //     }
+  //     else if (colorName.toLowerCase() === "verde") {
+  //       return "#00ff00";
+  //     }
+  //     else if (colorName.toLowerCase() === "negro") {
+  //       return "#000000";
+  //     }
+  //   };
+
   // Put your answer below -------------------------
-  var spanishColor = function(colorName) {
-    let colors = {
-        'rolo': '#ff0000',
-        'blanco': '#ffffff',
-        'azul': '#0000ff',
-        'verde': '#00ff00',
-        'negro': '#000000'
-    }
-    for (let prop in colors)
-        if(colorName.toLowerCase() === prop){
-            return colors[prop];
-        }
+  // let spanishColor = function(colorName) {
+  //   let colors = {
+  //       'rolo': '#ff0000',
+  //       'blanco': '#ffffff',
+  //       'azul': '#0000ff',
+  //       'verde': '#00ff00',
+  //       'negro': '#000000'
+  //   }
+  //   for (let prop in colors)
+  //       if(colorName.toLowerCase() === prop){
+  //           return colors[prop];
+  //       }
+  // };
+  // No need to loop through as the arugment will find the apropriate key. This could be optimized with .toLowerCase()
+  let colors = {
+    'rolo': '#ff0000',
+    'blanco': '#ffffff',
+    'azul': '#0000ff',
+    'verde': '#00ff00',
+    'negro': '#000000'
+  }
+  let spanishColor = function (colorName) {
+    return colorName.[color];
   };
 
 
-  
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 6. --------------------------------------------
-  
+
   // Below is a variable *declaration* and an
   // *assignment* in a single line of code.
   // Break it up so that the declaration and
   // assignment are happening on 2 seperate lines.
-  
-//   var foo = "bar";
-  
+
+  //   var foo = "bar";
+
   // Put your answer below -------------------------
   var foo;
   foo = 'bar';
-  
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 7. --------------------------------------------
-  
+
   // The function `callTenTimes` takes an argument
   // that is another function and will call that
   // function 10 times. Refactor this into another
   // function called `callNtimes` that allows you
   // to specify a number of times to call the given
   // function.
-  
-//   var callTenTimes = function(callback) {
-//     var range = Array.from(Array(10).keys());
-//     range.forEach(callback);
-//   };
-  
-//   callTenTime(functionName);
-  
+
+  //   var callTenTimes = function(callback) {
+  //     var range = Array.from(Array(10).keys());
+  //     range.forEach(callback);
+  //   };
+
+  //   callTenTime(functionName);
+
   // Put your answer below -------------------------
-  var callRunTimes = function(callback, runTimes) {
+  var callRunTimes = function (callback, runTimes) {
     var range = Array.from(Array(runTimes).keys());
     range.forEach(callback);
   };
-  
- 
-  
+
+
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 8. --------------------------------------------
-  
+
   // Below is the beginning code for an awesome game
   // but already suffers a vulnerability that allows
   // the savvy user to open the console and adjust
   // the score to whatever they want. Refactor the
   // code to protect from this.
-  
+
   // HINT: "global scope"
-  
-//   var score = 0;
-  
-//   var increaseScore = function() {
-//     score++;
-//   };
-  
-//   var decreaseScore = function() {
-//     score--;
-//   };
-  
+
+  //   var score = 0;
+
+  //   var increaseScore = function() {
+  //     score++;
+  //   };
+
+  //   var decreaseScore = function() {
+  //     score--;
+  //   };
+
   // Put your answer below -------------------------
-  let game = function(){
-      let score = 0;
-      let increaseScore = function() {
-        score = 0;
-        score++;
-      };
-      
-      let decreaseScore = function() {
-        score = 0;
-        score--;
-      };
+  let game = function () {
+    let score = 0;
+    let increaseScore = function () {
+      score = 0;
+      score++;
+    };
+
+    let decreaseScore = function () {
+      score = 0;
+      score--;
+    };
   }
-  
-  
-  
-  
+
+
+
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 9. --------------------------------------------
-  
+
   // The below function does not work. The variable
   // twoPlusTwo gets set to `undefined`. Refactor
   // the function to make it work.
-  
-//   var addNumbers = function(numberA, numberB) {
-//     console.log(numberA + numberB);
-//   };
-  
-//   var twoPlusTwo = addNumbers(2,2);
-  
+
+  //   var addNumbers = function(numberA, numberB) {
+  //     console.log(numberA + numberB);
+  //   };
+
+  //   var twoPlusTwo = addNumbers(2,2);
+
   // Put your answer below -------------------------
-  let addNumbers = function(numberA, numberB) {
+  let addNumbers = function (numberA, numberB) {
     let sum = numberA + numberB;
     return sum;
   };
-  
-  let twoPlusTwo = addNumbers(2,2);
-  
-  
-  
+
+  let twoPlusTwo = addNumbers(2, 2);
+
+
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // 10. -------------------------------------------
-  
+
   // Below is a snippet of code taken from a racing
   // game (not really!) It allows you to accelerate
   // the speed by a given amount. The problem is if
@@ -294,26 +304,26 @@
   // setting speed to NaN when no parameter is given
   // Then refactor the function to have a default
   // amount of 1 if no param is given.
-  
-//   var speed = 0;
-  
-//   var accelerate = function(amount) {
-//     speed += amount;
-//   };
-  
+
+  //   var speed = 0;
+
+  //   var accelerate = function(amount) {
+  //     speed += amount;
+  //   };
+
   // Put your answer below -------------------------
 
   //This comes back as Nan as you are trying to add 0 (a number) and amount (Nan) together. This results in a faley and will return NaN.
 
   var speed = 0;
-  
-  var accelerate = function(amount = 1) {
+
+  var accelerate = function (amount = 1) {
     speed += amount;
   };
-  
-  
+
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
   //            ___  ____  _  ____  ______        //
@@ -323,46 +333,46 @@
   //                                              //
   //////////////////////////////////////////////////
   //////////////////////////////////////////////////
-  
+
   // The function below allows you to call another
   // function at a later time. It takes 2 params, an
   // amount of miliseconds and a function. It will
   // call the function that many miliseconds later.
   // Refactor it so that is has a default timeout.
-  
+
   // This is more advanced than the default param on
   // the accelerate function. This is because there
   // is another parameter to consider.
-  
+
   // When setting the timeout, the function needs to
   // work like this:
-  
+
   //     callLater(1000, function(){
   //       ...
   //     });
-  
+
   // When using the default timeout, the function
   // needs to work like this:
-  
+
   //     callLater(function(){
   //       ...
   //     });
-  
-//   var callLater = function(timeout, callback) {
-//     setTimeout(callback, timeout);
-//   };
-  
+
+  //   var callLater = function(timeout, callback) {
+  //     setTimeout(callback, timeout);
+  //   };
+
   // Put your answer below -------------------------
-  var callLater = function(timeout = 1000, callback,) {
+  var callLater = function (timeout = 1000, callback,) {
     setTimeout(timeout, callback);
   };
 
-  
-  
+
+
   // -----------------------------------------------
-  
+
   //////////////////////////////////////////////////
-  })();
+})();
 //   © 2022 GitHub, Inc.
 //   Terms
 //   Privacy
@@ -375,4 +385,3 @@
 //   Training
 //   Blog
 //   About
-  
